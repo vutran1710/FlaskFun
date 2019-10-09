6c8dfd9f3e743b1e866db3088b51222f6a022de4
MaiToan71 <maivantoan339@gmail.com>
2019-10-09 17:26:58 +0700

    quesion 2 finshed

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ students = [{'name' : 'hung'},
         ]
 #  quesion 1 finshed                
 
-
-
-	
+@app.route('/students/<string:name>', methods=['GET'])
+def OneStudent(name):
+	name_students = [student for student in students  if student['name'] == name]
+	return jsonify({'student' : name_students[0]})
+#  quesion 2 finshed 
   
-    
 if __name__ == "__main__":
     app.run(debug=True)
