d6ac983a47e8dcf06baaa20a86ada8d6dd3008ae
MaiToan71 <maivantoan339@gmail.com>
2019-10-09 21:52:10 +0700

    edit

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 from flask_api import FlaskAPI 
-from flask import jsonify
 
 app = FlaskAPI(__name__)
 
-students = [{'name' : 'hung'}, 
-        {'name' : 'phong'}, 
-        {'name' : 'mai'},
-        {'name' : 'chi'},
-        {'name' : 'long'}
-        ]
-#  quesion 1 finshed                
+my_class = {
+        's1' : 'quoc',
+        's2' : 'phong',
+        's4' : 'long',
+        's4' : 'viet',
+        's5' : 'phuong'
+}
+#  quesion 1 finshed  
+@app.route('/', methods=['GET'])
+def ALlStudent():
+        return my_class             
 
-@app.route('/students/<string:name>', methods=['GET'])
-def OneStudent(name):
-	name_students = [student for student in students  if student['name'] == name]
-	return jsonify({'student' : name_students[0]})
+@app.route('/s1', methods=['GET'])
+def student_1():
+        return my_class['s1']
+
+@app.route('/s2', methods=['GET'])
+def student_2():
+        return my_class['s2']
+
+       
+                
     
 #  quesion 2 finshed 
   
