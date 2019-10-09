5c1971da016f67c83086d65318ea912c4f3833fe
yamadaken93 <yamadaken93@gmail.com>
2019-10-10 02:03:15 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask_api import FlaskAPI
+from flask_api import FlaskAPI 
+from flask import jsonify
 
 app = FlaskAPI(__name__)
 
+students = [{'name' : 'hung'}, 
+        {'name' : 'phong'}, 
+        {'name' : 'mai'},
+        {'name' : 'chi'},
+        {'name' : 'long'}
+        ]
+#  quesion 1 finshed                
 
+@app.route('/students/<string:name>', methods=['GET'])
+def OneStudent(name):
+	name_students = [student for student in students  if student['name'] == name]
+	return jsonify({'student' : name_students[0]})
+    
+@app.route('/students', methods=['POST'])
+def addstudent():
+    if  request.methods == 'POST': 
+        student = {'name': request.json['name']}
+        students.append(student)
+        return { 'name' : students}
+    else
+        return "Lỗi rồi bạn ơi"
+
+# question 4
 if __name__ == "__main__":
     app.run(debug=True)
