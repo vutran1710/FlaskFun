bfa665bbd0cf32f5bdd8ab4f9988573d0f2fa871
MaiToan71 <maivantoan339@gmail.com>
2019-10-09 17:20:19 +0700

    quesion 1 finshed

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask_api import FlaskAPI
+from flask_api import FlaskAPI 
+from flask import jsonify
 
 app = FlaskAPI(__name__)
 
+students = [{'name' : 'hung'}, 
+        {'name' : 'phong'}, 
+        {'name' : 'mai'},
+        {'name' : 'chi'},
+        {'name' : 'long'}
+        ]
+#  quesion 1 finshed                
 
+
+
+	
+  
+    
 if __name__ == "__main__":
     app.run(debug=True)
