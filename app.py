2e5b966f29a68415d6b7e8e6432028d2acf24fdd
yamadaken93 <yamadaken93@gmail.com>
2019-10-10 14:33:22 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,32 @@
 from flask_api import FlaskAPI 
-from flask import jsonify
+from flask import request
 
 app = FlaskAPI(__name__)
 
-students = [{'name' : 'hung'}, 
-        {'name' : 'phong'}, 
-        {'name' : 'mai'},
-        {'name' : 'chi'},
-        {'name' : 'long'}
-        ]
-#  quesion 1 finshed                
+RESULT = {
+    'language' : 'Python',
+    'framework' : 'Flask',
+    'website' : 'Facebook',
+    'editor' : 'vscode'        
+}
 
-@app.route('/students/<string:name>', methods=['GET'])
-def OneStudent(name):
-	name_students = [student for student in students  if student['name'] == name]
-	return jsonify({'student' : name_students[0]})
+@app.route('/', methods=['GET'])
+def get_value():
+    key = request.args.get('key')
     
-@app.route('/students', methods=['POST'])
-def addstudent():
-        student = {'name': request.json['name']}
-        students.append(student)
-        return { 'name' : students}
+    if not key :
+        return "You forgot key"
 
-# question 4
+    return "value: %s " % (RESULT[key])
+
+@app.route('/addvalue', methods=['POST'])
+def add_value():
+    if request.method == 'POST' and request.is_json:
+        RESULT.update(request.json)
+        return RESULT
+    else:
+        return "Fail add value"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
