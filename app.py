85cd096e5247f4adedaca24bd1508c8d7bf38cb6
MaiToan71 <maivantoan339@gmail.com>
2019-10-10 10:56:44 +0700

    em vừa fix lại, e xem giúp em

--- app.py
+++ app.py
@@ -4,19 +4,20 @@ from flask import request
 app = FlaskAPI(__name__)
 
 RESULT = {
-        's1' : 'value1',
-        's2' : 'value2',
-        's3' : 'value3',
-        's4' : 'value4',
-        's5' : 'value5'
+    'language' : 'Python',
+    'framework' : 'Flask',
+    'website' : 'Facebook',
+    'editor' : 'vscode'        
 }
-#  quesion 1 finshed  
-@app.route('/data', methods=['GET'])
-def query_string():
-    args = request.args['key']       
-    return RESULT[args]
-
-#  quesion 2 finshed 
   
+@app.route('/', methods=['GET'])
+def get_value():
+    key = request.args.get('key')
+    if not key :
+        return "You forgot key"
+
+    return "value: %s " % (RESULT[key])
+  
+
 if __name__ == "__main__":
     app.run(debug=True)
