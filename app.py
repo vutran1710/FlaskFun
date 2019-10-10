8c6be274e9e73c6e7ac7a5dc2149369fa1f13db3
nguyenvanson2201 <n.vanson.2201@gmail.com>
2019-10-10 10:13:57 +0700

    edit1

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask_api import FlaskAPI
+from flask import request
 
 app = FlaskAPI(__name__)
 
 @app.route('/data', methods=['PATCH'])
 def data():
     req = request.get_json()
+    
+    key = list(req)[0]
+    old_value = RESULT[key]
+    new_value = req[key]
+
     RESULT.update(req)
 
-    return "Successful updated!"
+    return 'key {} has successful updated from old value: {} to new value: {}'.format(key, old_value, new_value)
 
 if __name__ == "__main__":
     app.run(debug=True)
