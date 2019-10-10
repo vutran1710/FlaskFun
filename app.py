15f2325d84886aae1372d156d1cad09c79f298f7
nguyenvanson2201 <n.vanson.2201@gmail.com>
2019-10-10 09:04:35 +0700

    add PATCH method

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask_api import FlaskAPI
 
 app = FlaskAPI(__name__)
 
+@app.route('/data', methods=['PATCH'])
+def data():
+    req = request.get_json()
+    RESULT.update(req)
+
+    return "Successful updated!"
 
 if __name__ == "__main__":
     app.run(debug=True)
