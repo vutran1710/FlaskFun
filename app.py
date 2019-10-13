b9896008ebc443501e9c9e6232018fe14f4ef376
MaiToan71 <maivantoan339@gmail.com>
2019-10-13 16:53:57 +0700

    demo

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask_api import FlaskAPI 
-from flask import request
+from flask import request, abort
 
 
 app = FlaskAPI(__name__)
@@ -16,49 +16,17 @@ def get_value():
     key = request.args.get('key')
     
     if not key:
-        return "You forgot key"
+        abort(404)
     
     if key not in RESULT:
-        return "Key does not exist"
+         abort(404)
            
-    return "value: %s " % (RESULT[key])   
+    return "value: %s " % (RESULT[key])  
+
+@app.errorhandler(404) 
+def not_found(error):
+    return "Key does not exist",404
   
-@app.route('/data', methods=['PATCH'])
-def update_key():
-    key = request.args.get('key')
-    request_json_body = request.get_json()
-
-    if key not in RESULT:
-        return "Key does not exist"
-
-    if not request.is_json:
-        return "Invalid: content type is not json"
-
-    if "value" not in request_json_body:
-        return "request body does have key named value!"
-
-    old_value = RESULT[key]
-    new_value = request_json_body['value']
-    RESULT.update({ key: new_value })
-
-    return "key {} has successful updated " \
-           "from old value: {} " \
-           "to new value: {}".format(key, old_value, new_value)
-
-@app.route('/data', methods=['DELETE'])
-def delete_key():
-    key = request.args.get('key')
-
-    if not key:
-        return "You forgot key"
-    
-    if key not in RESULT:
-        return "key does not exist"
-
-    del RESULT[key]
-
-    return "key: {} has been deleted!".format(key)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
