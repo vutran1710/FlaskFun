e780b26a7bc4fa1164c508563b031a5a229fc686
nguyenvanson2201 <n.vanson.2201@gmail.com>
2019-10-20 23:46:04 +0700

    test user api

--- test/test_api/test_simple_data.py
+++ test/test_api/test_simple_data.py
@@ -1,30 +1,16 @@
-import sys
-sys.path.append('../')
+import json
 from app import create_app
 
 
 app = create_app()
 
 
-def test_get():
-    response = app.test_client().get('/api/user?key=editor')
+def test_post():
+    payload = {"name": "Viet", "email": "n.vanviet.2209@gmail.com"}
+    response = app.test_client().post('/api/user',
+                                      data=json.dumps(payload),
+                                      content_type='application/json',)
 
     assert response.status_code == 200
-    assert response.data == b'value: vscode '
-
-
-def test_put():
-    response = app.test_client().put('/api/simple_data?key=java')
-    assert response.status_code == 200
-    assert response.data == b"New keys with empty string values added: java"
-
-
-def test_delete():
-    response = app.test_client().delete('/api/simple_data?key=language')
-    assert response.status_code == 200
-    assert response.data == b'key: language has been deleted!'
-
-# def test_post():
-#     response = app.test_client().delete('/api/simple_data?key=language')
-#     assert response.status_code == 200
-#     assert response.data == b'New keys added: '
+    response_body = response.get_json()
+    assert response_body['added_user']['email'] == payload['email']
