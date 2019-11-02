6f71cd812123ac2a57cd15c29ccdfdb4de35823c
nguyenvanson2201 <n.vanson.2201@gmail.com>
2019-11-02 16:47:26 +0700

    add Send-Email-Confirmation

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,13 @@
 DEBUG = True
 TESTING = False
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5422/postgres"
+
+SECRET_KEY = 'LUHAWKFLUAH((*%%@$#DHA))'
+
+
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'your-email'
+MAIL_PASSWORD = 'your-password'
+MAIL_DEFAULT_SENDER = 'your-email'
