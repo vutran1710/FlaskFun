349a72fae91f421b84283fd4e44e67d77ced1837
nguyenvanson2201 <n.vanson.2201@gmail.com>
2019-11-23 00:40:01 +0700

    add test for login logout

---
+++ app/test/test_api/test_login.py
@@ -0,0 +1,27 @@
+import json
+from app import bcrypt
+
+
+def test_login(app):
+    data_sended = {"name": "Lam", "password": "1234567cC"}
+    response = app.test_client().post('/api/login',
+                                      data=json.dumps(data_sended),
+                                      content_type='application/json',)
+    assert response.status_code == 200
+    response_body = response.get_json()
+    assert response_body['message'] == "Login success."
+    assert response_body['user']['username'] == data_sended['name']
+    assert bcrypt.check_password_hash(response_body['user']['password'], data_sended['password'])
+
+
+def test_logout(app):
+    data_sended = {"name": "Lam", "password": "1234567cC"}
+    resp_login = app.test_client().post('/api/login',
+                                        data=json.dumps(data_sended),
+                                        content_type='application/json',)
+    headers = {
+        'Authorization': 'Bearer {}'.format(resp_login.get_json()['token'])
+    }
+    response = app.test_client().delete('/api/logout', headers=headers)
+    response_body = response.get_json()
+    assert response_body['message'] == "Successfully logged out."
