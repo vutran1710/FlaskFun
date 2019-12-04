8be9a5b96a769266ae8c8cb997b5ccd48c94132e
sonnguyen2201 <n.vanson.2201@gmail.com>
2019-12-04 22:48:03 +0700

    fix clients

--- async_request_test/clients.py
+++ async_request_test/clients.py
@@ -5,37 +5,40 @@ import aiohttp
 import matplotlib.pyplot as plt
 
 begin = time.time()
-start_times = []
 end_times = []
+waiting_after_10s = 0
 
 async def fetch(session, url, task_name):
     global begin
-    print( 'Start time of {} is: {}'.format(task_name, time.strftime("%X")))
-    start_times.append(time.time() - begin)
+    global end_times
+    global waiting_after_10s
+
     async with session.get(url) as response:
-        return await response.json()
+        await response.json()
+        if time.time() - begin > 10:
+            print("Over 10s")
+            waiting_after_10s += 1
+        end_times.append(time.time() - begin)
+
 
 async def main(num):
     global begin
     my_url = 'http://192.168.0.103:5000/api/user/1'
     my_session = aiohttp.ClientSession()
-    task_lists = [fetch(my_session, my_url, 'task {}'.format(i+1)) for i in range(num)]
+    tasks = [fetch(my_session, my_url, 'task {}'.format(i+1)) for i in range(num)]
+    for task in await asyncio.gather(*tasks):
+        pass
 
-    j = 0
-    for task in await asyncio.gather(*task_lists):
-        print( 'End time of task {} is '.format(j+1) + time.strftime("%X"))
-        end_times.append(time.time() - begin)
-        j+=1
+    requests = [i for i in range (num)]
+
+    fig = plt.figure()
+    plt.plot(requests, end_times, marker="o", markersize=0.4)
+    plt.xlabel("request")
+    plt.ylabel("time")
+    fig.savefig('async.png')
     
-    responses = [i for i in range (num)]
-
-    fig, ax = plt.subplots()
-    ax.scatter(responses, start_times, marker="o", s=0.4)
-    ax.set_xlabel("response")
-    ax.set_ylabel("time")
-    ax.scatter(responses, end_times, marker="o", s=0.4)
-    plt.show()
-
+    global waiting_after_10s
+    print(waiting_after_10s)
 
 asyncio.run(main(1000))
 
